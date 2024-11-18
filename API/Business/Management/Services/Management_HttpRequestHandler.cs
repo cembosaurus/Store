85affd68d41a350f6898952aa5aa6ed290819c5e
Milos Bencek <milos.bencek@gmail.com>
2024-11-18 13:12:37 +1100

    18112024_2

---
+++ API/Business/Management/Services/Management_HttpRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Management.Services
+{
+    public class Management_HttpRequestHandler : DelegatingHandler
+    {
+
+        private HttpResponseMessage _responseMessage;
+
+
+
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage requestMessage, CancellationToken cancellationToken)
+        {
+            _responseMessage = await base.SendAsync(requestMessage, cancellationToken);
+
+            return _responseMessage;
+        }
+
+
+
+
+    }
+}
