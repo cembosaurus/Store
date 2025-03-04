e927e27cd3a6d7393a15e194f22db4b23ab42c57
Milos Bencek <milos.bencek@gmail.com>
2025-03-04 11:53:54 +1100

    04032025_1_Paging

--- API/Business/Middlewares/RequestHandler_MW.cs
+++ API/Business/Middlewares/RequestHandler_MW.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Management.Appsettings.Interfaces;
+using Business.Management.Services;
 using Business.Management.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Primitives;
@@ -17,6 +18,11 @@ namespace Business.Middlewares
     {
         private readonly RequestDelegate _next;
 
+
+        private const int _defaultPageNumber = 1;
+        private const int _defaultPageSize = 20;
+
+
         public RequestHandler_MW(RequestDelegate next)
         {
                 _next = next;
@@ -27,14 +33,20 @@ namespace Business.Middlewares
         public async Task InvokeAsync(HttpContext context, IGlobalConfig_PROVIDER globalConfig_Provider)
         {
 
+            Pagging(context, globalConfig_Provider);
+
+            await _next(context);
+        }
 
 
 
 
+        private void Pagging(HttpContext context, IGlobalConfig_PROVIDER globalConfig_Provider)
+        {
 
             // nefunguje to ked deletnem v VS Code appsettings persistance. Data su poslane z Managementu ale niuesu potom prepisane v Ordering GC DB !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-
+            // !!!! order runs alobe -> default OK. After starting Management -> Order GC NOT updated!!!!!!
 
 
 
@@ -42,20 +54,34 @@ namespace Business.Middlewares
             // To Do: catch SQL error if page number out of range
 
 
-            var gcResult = globalConfig_Provider.GetPersistence();
-            var gcData = gcResult.Status && gcResult.Data!.Pagination.DefaultPageNumber > 0 && gcResult.Data.Pagination.DefaultPageSize > 0 ? gcResult.Data : new () { Pagination = new() { DefaultPageNumber = 90, DefaultPageSize = 3} };
-            var defaultPageNumber = gcData!.Pagination.DefaultPageNumber;
-            var defaultPageSize = gcData!.Pagination.DefaultPageSize;
+            // Get pagging from request:
+            var pageStr = context.Request.Query["page"] == StringValues.Empty ? _defaultPageNumber.ToString() : context.Request.Query["page"][0];
+            var sizeStr = context.Request.Query["size"] == StringValues.Empty ? _defaultPageSize.ToString() : context.Request.Query["size"][0];
 
-            var page = context.Request.Query["page"] == StringValues.Empty ? defaultPageNumber.ToString() : context.Request.Query["page"][0];
-            var size = context.Request.Query["size"] == StringValues.Empty ? defaultPageSize.ToString() : context.Request.Query["size"][0]; ;
+            if (!(int.TryParse(pageStr, out var pageInt) && pageInt > 0
+                && int.TryParse(sizeStr, out var sizeInt) && sizeInt > 0))
+            {
+                // If pagging not found in request, get pagging from GC:
+                var gcResult = globalConfig_Provider.GetPersistence();
+                var gcData = gcResult.Status && gcResult.Data!.Pagination.DefaultPageNumber > 0 && gcResult.Data.Pagination.DefaultPageSize > 0
+                    ? gcResult.Data
+                    : new()
+                    {
+                        Pagination = new()
+                        {
+                            DefaultPageNumber = _defaultPageNumber,
+                            DefaultPageSize = _defaultPageSize
+                        }
+                    };
 
-            context.Items.Add("page", page);
-            context.Items.Add("size", size);
+                pageInt = gcData!.Pagination.DefaultPageNumber;
+                sizeInt = gcData!.Pagination.DefaultPageSize;
+            }
 
-            await _next(context);
+            context.Items.Add("page", (pageInt - 1) * sizeInt);
+            context.Items.Add("size", sizeInt);
+
         }
-
 
 
     }
